b3f146df3fddbaa15859859ddacdc51ab466aa87
Wai Hwa Tan <twaih92@gmail.com>
2015-09-09 22:00:06 -0700

    get request returns typeError

--- client/search.js
+++ client/search.js
@@ -1,8 +1,19 @@
 function handleSearch(){
   var search_request = document.getElementById("searchbar").value;
-  //console.log(search_request);
-  $.post('/search',search_request).then(function(result){
-    console.log(success);
+  console.log(search_request);
+  var request = {};
+  request.query = search_request;
+  $.post('/search',request).then(function(result){
+    console.log(result);
   });
 
+  // var request = gapi.client.youtube.search.list({
+  //   q: search_request,
+  //   part: 'snippet'
+  // });
+
+  // request.execute(function(response){
+  //   var str = JSON.stringify(response.result);
+  //   console.log('str is ', str);
+  // });
 }
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var $ = require('jquery');
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.post('/search', function(req,res){
-  console.log(req.body);
+  console.log('received post request');
+  console.log(req.body.query);
+
+  $.getJSON('http://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q='+
+   req.body.query+'&key=AIzaSyCuEqJLhJwSPBWtn2rJ6kF5f5VaP6g7mAw', function(data){
+     console.log('data is',data);
+   });
+  // $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q='+
+  // req.body.query+'&key=AIzaSyCuEqJLhJwSPBWtn2rJ6kF5f5VaP6g7mAw',null,function(data){
+  //   console.log('got data');
+  // });
+
 });
 
 app.listen(3000);
